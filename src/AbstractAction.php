63b76b33c120ec0167ee84b29b00f7e20d2d94e5
WouterWessendorp <w.wessendorp@vertaalbureau-perfect.nl>
2021-05-26 09:36:58 +0200

    Add is executed and is executedExactly

--- src/AbstractAction.php
+++ src/AbstractAction.php
@@ -8,19 +8,41 @@ use Illuminate\Container\Container;
 
 abstract class AbstractAction
 {
+
+    protected static int $times_fake_executed = 0;
+
+    protected static $fake;
+
     final public static function fake($returnValue = null)
     {
         $fake = new ActionFake();
         $fake->returnValue = $returnValue;
+
+        self::$fake = $fake;
 
         Container::getInstance()->instance(static::class, $fake);
     }
 
     final public static function execute(...$parameters)
     {
+        if (self::$fake) {
+            self::$times_fake_executed++;
+        }
+
         return call_user_func_array([
             resolve(static::class),
             'handle'
         ], $parameters);
     }
+
+    public static function isExecuted(): bool
+    {
+        return self::$times_fake_executed > 0;
+    }
+
+    public static function isExecutedExactly(int $times): bool
+    {
+        return $times === self::$times_fake_executed;
+    }
+
 }
